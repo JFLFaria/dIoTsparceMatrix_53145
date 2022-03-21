8cc5273b4e3ecbd77780a698e70b7e66112ec889
JFLFaria <80894007+JFLFaria@users.noreply.github.com>
2022-03-21 14:51:54 +0000

    Add files via upload

---
+++ position.py
@@ -0,0 +1,25 @@
+# (int, int)
+def position_create(row, col):
+    return False
+
+
+# (position)
+def position_is(pos):
+    return False
+
+
+# (position)
+def position_row(pos):
+    return False
+
+
+def position_col(pos):
+    return False
+
+
+def position_equal(pos, pos2):
+    return False
+
+
+def position_str(pos):
+    return False
---
+++ spmatrix.py
@@ -0,0 +1,64 @@
+# (float)
+def spmatrix_create(zero):
+    return False
+
+
+# (spmatrix)
+def spmatrix_is(mat):
+    return False
+
+
+# (spmatrix)
+def spmatrix_zero_get(mat):
+    return False
+
+
+# (spmatrix, float)
+def spmatrix_zero_set(mat, zero):
+    return False
+
+
+# (spmatrix, float)
+def spmatrix_value_get(mat, pos):
+    return False
+
+
+# (spmatrix, position, float)
+def spmatrix_value_set(mat, pos, val):
+    return False
+
+
+# (spmatrix)
+def spmatrix_copy(mat):
+    return False
+
+
+# (spmatrix)
+def spmatrix_dim(mat):
+    return False
+
+
+# (spmatrix)
+def spmatrix_sparsity(mat):
+    return False
+
+
+# (spmatrix)
+def spmatrix_str(mat):
+    return False
+
+
+# (spmatrix)
+def spmatrix_row(mat):
+    return False
+
+
+# (spmatrix)
+def spmatrix_col(mat):
+    return False
+
+
+# (spmatrix)
+def spmatrix_diagonal(mat):
+    return False
+
