84d5a46b086fe7c9d5d5e41fed19ea6ab97e0fb1
jfran <jfl.faria@campus.fct.unl.pt>
2022-03-28 14:48:43 +0100

    test files created

--- positionTest.py
+++ positionTest.py
@@ -26,3 +26,31 @@ def test_position_create_invalid_args_emptytuple():
 
 def test_position_is_true():
     assert position_is(position_create(1, 2)) is True
+
+
+def test_position_is_false():
+    assert position_is(1.2) is False
+
+
+def test_position_row():
+    assert position_row(position_create(1, 2)) == 1
+
+
+def test_position_col():
+    assert position_col(position_create(1, 2)) == 2
+
+
+def test_position_equal_same():
+    assert position_equal(position_create(1, 2), position_create(1, 2)) is True
+
+
+def test_position_equal_different():
+    assert position_equal(position_create(1, 2), position_create(2, 1)) is False
+
+
+def test_position_str():
+    assert position_str(position_create(1, 2)) == '(1, 2)'
+
+
+if __name__ == '__main__':
+    pytest.main()
