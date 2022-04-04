2306115d11c3f62fd1d468ec84f500dd12e15dea
jfran <jfl.faria@campus.fct.unl.pt>
2022-04-04 15:23:07 +0100

    wrote a few more functions

--- spmatrix.py
+++ spmatrix.py
@@ -1,18 +1,19 @@
 from position import *
 
-dok = {}
+
+# dok = {}
 
 
 # (float)
 def spmatrix_create(zero: float = 0.0):
     if not (type(zero) is float):
         raise ValueError('position_create: invalid arguments')
-    return zero, dok
+    return zero, {}
 
 
 # (spmatrix)
 def spmatrix_is(mat):
-    if type(mat) is tuple and type(mat[0]) is float and type[1] is dict:
+    if type(mat) is tuple and type(mat[0]) is float and type(mat[1]) is dict:
         return True
     else:
         return False
@@ -47,39 +48,72 @@ def spmatrix_value_get(mat, pos):
 
 # (spmatrix, position, float)
 def spmatrix_value_set(mat, pos, val):
-    return False  # TODO: Continue
+    if spmatrix_is(mat) and type(pos) is tuple and type(val) is float:
+        mat[1][pos] = val
+    else:
+        raise ValueError("spmatrix_value_set: invalid arguments")
 
 
 # (spmatrix)
 def spmatrix_copy(mat):
-    return False
+    if spmatrix_is(mat):
+        new_mat = ()
+        # TODO: Copy the elements now
+        return new_mat
+    else:
+        raise ValueError("spmatrix_copy: invalid arguments")
 
 
 # (spmatrix)
 def spmatrix_dim(mat):
-    return False
+    if spmatrix_is(mat):
+        if mat[1]:
+            max_row = max(mat[1].keys(), key=lambda item: item[0])[0]
+            max_col = max(mat[1].keys(), key=lambda item: item[1])[1]
+            min_row = min(mat[1].keys(), key=lambda item: item[0])[0]
+            min_col = min(mat[1].keys(), key=lambda item: item[1])[1]
+            return position_create(min_row, min_col), position_create(max_row, max_col)
+        else:
+            return ()
+    else:
+        raise ValueError("spmatrix_dim: invalid arguments")
 
 
 # (spmatrix)
 def spmatrix_sparsity(mat):
-    return False
+    if spmatrix_is(mat):
+        return len(mat[1]) / spmatrix_dim(mat)
+    else:
+        raise ValueError("spmatrix_sparsity: invalid arguments")
 
 
 # (spmatrix)
 def spmatrix_str(mat):
-    return False
+    if spmatrix_is(mat):
+        mat[1]  # TODO: Tirar duvida (ordenar as posições?)
+    else:
+        raise ValueError("spmatrix_str: invalid arguments")
 
 
 # (spmatrix)
 def spmatrix_row(mat):
-    return False
+    if spmatrix_is(mat):
+        mat[1]  # TODO: Tirar duvida
+    else:
+        raise ValueError("spmatrix_row: invalid arguments")
 
 
 # (spmatrix)
 def spmatrix_col(mat):
-    return False
+    if spmatrix_is(mat):
+        mat[1]  # TODO: Tirar duvida
+    else:
+        raise ValueError("spmatrix_col: invalid arguments")
 
 
 # (spmatrix)
 def spmatrix_diagonal(mat):
-    return False
+    if spmatrix_is(mat):
+        mat[1]  # TODO: Tirar duvida
+    else:
+        raise ValueError("spmatrix_diagonal: invalid arguments")
